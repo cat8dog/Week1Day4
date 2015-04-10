de506f7e52a46aafe3ba5ce7f0db89c0e6a11ecc
cat8dog <catherine.reyto@gmail.com>
2015-04-10 13:28:50 -0400

    updating main.m

--- Car/main.m
+++ Car/main.m
@@ -1,17 +1,17 @@
-//
-//  main.m
-//  Car
-//
-//  Created by Catherine Reyto on 2015-04-10.
-//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import "Car.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Car *toyota = [[Car alloc] init];
+        
+        [toyota setModel:@"Toyota Corolla"];
+        NSLog(@"Created a %@", [toyota model]);
+        
+        toyota.model = @"Toyota Camry";
+        NSLog(@"Changed the car to a %@", toyota.model);
+        
+        [toyota drive];
     }
     return 0;
 }
