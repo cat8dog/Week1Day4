057440c6dbe30995c4dc6fc1be94c15bc55ede61
cat8dog <catherine.reyto@gmail.com>
2015-04-10 12:40:32 -0400

    Initial Commit

---
+++ Car/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Car
+//
+//  Created by Catherine Reyto on 2015-04-10.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
